872372306f106c22e99014d4c5be45a111ad7b21
nada <nada50tarek@gmail.com>
2023-10-25 17:31:58 +0300

    add text sliding animation

--- lib/Features/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/view/widgets/splash_view_body.dart
@@ -1,8 +1,37 @@
 import 'package:book_store/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2.6),
+      end: Offset.zero,
+    ).animate(controller);
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,12 +44,15 @@ class SplashViewBody extends StatelessWidget {
           height: 200,
           width: 200,
         ),
-        const SizedBox(
-          height: 7,
-        ),
-        const Text(
-          'BOOK STORE',
-          textAlign: TextAlign.center,
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context, _) => SlideTransition(
+            position: slidingAnimation,
+            child: const Text(
+              'BOOK STORE',
+              textAlign: TextAlign.center,
+            ),
+          ),
         ),
       ],
     );
